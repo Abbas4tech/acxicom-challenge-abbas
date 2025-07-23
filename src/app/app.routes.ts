058b923bf1952b89027e195345e5f3adeb2a2c2b
Abbas4tech <shaikhabbas54@gmail.com>
2025-07-23 06:51:28 +0530

    ✨ feat: create sample login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginScreen } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginScreen },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+];
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,71 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// Material imports
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'auth-screen',
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.scss',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  standalone: true,
+})
+export class LoginScreen {
+  loginForm: FormGroup<{
+    username: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
+
+  hide = signal(true);
+
+  constructor(private _fb: FormBuilder) {
+    this.loginForm = this.createAuthForm();
+  }
+
+  private createAuthForm() {
+    return this._fb.group<{
+      username: FormControl<string | null>;
+      password: FormControl<string | null>;
+    }>({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  togglePasswordShow(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  onSubmit() {
+    console.log('Form Submitted', this.loginForm.value);
+  }
+
+  clearForm() {
+    this.loginForm.reset();
+    this.loginForm.markAsPristine();
+    this.loginForm.markAsUntouched();
+  }
+}
